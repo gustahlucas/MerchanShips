0012b8f9a3cfa311e774551e2ea659d786e506c5
Gustavo Lucas Moreira <gustahlucasstudent@users.noreply.github.com>
2020-09-15 19:42:10 -0300

    Ilhas

---
+++ Assets/FlutuarObjeto.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+public class FlutuarObjeto : MonoBehaviour
+{
+    private float Seno = 0.0f;
+    private int vez = 0;
+    private float cronometro = 0.0f;
+    private float MovimentoEmX;
+    private float TorqueEmX;
+    private float TorqueEmY;
+    private float TorqueEmZ;
+    public float VelocidadeVertical = 0;
+    public float DistanciaVertical = 0;
+    public float VelocidadeHorizontal = 0;
+    public float VelocidadeDeRotacao = 0;
+    void Start()
+    {
+        MovimentoEmX = VelocidadeHorizontal;
+        TorqueEmX =VelocidadeDeRotacao;
+        TorqueEmY =  VelocidadeDeRotacao;
+        TorqueEmZ = VelocidadeDeRotacao;
+        GetComponent<Rigidbody>().AddRelativeTorque(new Vector3(TorqueEmX, TorqueEmY, TorqueEmZ));
+    }
+    void FixedUpdate()
+    {
+        if (Seno < Mathf.PI && vez == 0)
+        {
+            Seno += Time.deltaTime;
+        }
+        if (Seno >= Mathf.PI)
+        {
+            vez = 1;
+        }
+        if (Seno <= 0)
+        {
+            vez = 0;
+        }
+        if (Seno >= 0 && vez == 1)
+        {
+            Seno = 0;
+        }
+        GetComponent<Rigidbody>().velocity = new Vector3(MovimentoEmX, Mathf.Sin(2 * Seno * VelocidadeVertical) * DistanciaVertical, 0);
+        if (cronometro < 10)
+        {
+            cronometro += Time.deltaTime;
+        }
+        if (cronometro >= 10)
+        {
+            cronometro = 0;
+            GetComponent<Rigidbody>().AddRelativeTorque(new Vector3(TorqueEmX, TorqueEmY, TorqueEmZ));
+        }
+    }
+}
